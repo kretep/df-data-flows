81db445cf13044ea7aea5be96c5fabd67b9f427b
Peter Kok <kretep@users.noreply.github.com>
2025-07-27 21:39:13 +0200

    Add p1_meter / homewizard etl

--- flows/homewizard.py
+++ flows/homewizard.py
@@ -10,13 +10,25 @@ from common.database_utils import write_to_database
 @task
 def fetch_homewizard_data():
     url = os.getenv("HW_P1_ENDPOINT")
-    data = fetch_json.get(url)
+    data = fetch_json(url)
     return data
 
 @task
 def process_homewizard_data(data):
     # Filter the keys that we want
-    keys = ['total_power_export_t1_kwh', 'total_power_export_t2_kwh', 'total_power_import_t1_kwh', 'total_power_import_t2_kwh', 'active_power_w']
+    keys = [
+        'active_tariff',
+        'total_power_import_kwh',
+        'total_power_import_t1_kwh',
+        'total_power_import_t2_kwh',
+        'total_power_export_kwh',
+        'total_power_export_t1_kwh',
+        'total_power_export_t2_kwh',
+        'active_power_w',
+        'active_power_l1_w',
+        'active_power_l2_w',
+        'active_power_l3_w',
+    ]
     data = { key: data[key] for key in keys }
     return data
 
@@ -30,7 +42,6 @@ def store_homewizard_data(data):
 @flow(name="Homewizard data ETL")
 def homewizard_etl():
     data = fetch_homewizard_data()
-    print(f"Fetched data: {data}")
     processed_data = process_homewizard_data(data)
     store_homewizard_data(processed_data)
 
---
+++ sql/create_table_p1_meter.pgsql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS public.p1_meter
+(
+    created_at timestamp with time zone NOT NULL DEFAULT 'now()',
+    active_tariff integer NOT NULL,
+    total_power_import_kwh real NOT NULL,
+    total_power_import_t1_kwh real NOT NULL,
+    total_power_import_t2_kwh real NOT NULL,
+    total_power_export_kwh real NOT NULL,
+    total_power_export_t1_kwh real NOT NULL,
+    total_power_export_t2_kwh real NOT NULL,
+    active_power_w real NOT NULL,
+    active_power_l1_w real NOT NULL,
+    active_power_l2_w real NOT NULL,
+    active_power_l3_w real NOT NULL,
+
+    CONSTRAINT p1_meter_pkey PRIMARY KEY (created_at)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.p1_meter
+    OWNER to postgres;
